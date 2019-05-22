ef1db910bddabbf64292b32fb4b21c48391dc226
Wang Chenxu <git@wangchenxu.net>
2019-05-22 18:06:13 +0800

    feat(service-worker): 增加缓存项并优化缓存管理

--- service-worker.js
+++ service-worker.js
@@ -1,3 +1,16 @@
+const itemVersion = {
+
+};
+
+const baseItem = {
+	'init.html': 0,
+	'xutool.html': 0,
+	'pwa.html': 0,
+	'script.js': 0,
+	'style.css': 0,
+}
+
+
 const serviceRoot = self.location.href.replace(/\/*([^/]*)(?:[#?].*)?$/, '/');
 function getPath(url) {
 	if (url.indexOf(serviceRoot) !== 0) { return; }
@@ -7,31 +20,60 @@ function getPageId(path) {
 	path = /^([a-z0-9\-]*)(?:\.html)?$/.exec(path);
 	if (path) { return path[1] || 'index'; }
 }
-function getUtilId(path) {
-	path = /^util\/([a-z0-9\-]*)\.js$/.exec(path);
+function getItemId(path) {
+	path = /^((?:component|util)\/[a-z0-9\-]*\.js)$/.exec(path);
+	if (path) { return path[1] || ''; }
+	path = /^(logo\/\d+\.png)$/.exec(path);
+	if (path) { return path[1] || ''; }
+	path = /^(logo\/logo\.svg)$/.exec(path);
+	if (path) { return path[1] || ''; }
+	path = /^(favicon\.ico)$/.exec(path);
 	if (path) { return path[1] || ''; }
 }
-function getComponentId(path) {
-	path = /^component\/([a-z0-9\-]*)\.js$/.exec(path);
-	if (path) { return path[1] || ''; }
-}
-const pageId = 1;
-self.addEventListener('install', function(event) {
-	event.waitUntil(caches.open('page-' + pageId).then(cache => {
-		return cache.addAll([
-			'./init.html',
-			'./xutool.html',
-			'./pwa.html',
-			'./script.js',
-			'./style.css',
-		])
-	}));
-});
 
+async function putItem(id, res, cache = 'xutool-item', version = itemVersion[id] || 0) {
+	const data = await res.clone().blob();
+	res = new Response(data, {
+		headers: {
+			'Content-Type': data.type,
+			'Xu-Tools-Version': version,
+			'Content-Length': data.size,
+		}
+	})
+	cache = await caches.open(cache)
+	cache.put(`./${id}`, res);
+}
+async function backup(id, cache = 'xutool-item', version = itemVersion[id]) {
+	const res = await fetch(`./${id}`);
+	if(res.status !== 200) { return res; }
+	putItem(id, res, cache, version);
+	return res;
+}
+async function updateItem(id, oldRes, cache = 'xutool-item', version = itemVersion[id]) {
+	if (typeof version !== 'number') { return oldRes; }
+	if (oldRes && Number(oldRes.headers.get('Xu-Tools-Version') || 0) >= version) { return oldRes; }
+	try {
+		const res = await fetch(`./${id}`);
+		if(res.status !== 200) { return oldRes; }
+		putItem(id, res, cache, version);
+		return res;
+	} catch(e) {
+		return oldRes;
+	}
+
+}
+
+
+self.addEventListener('install', function(event) {
+	event.waitUntil(caches.open('xutool-base-page')
+		.then(cache => Promise.all(
+			Object.keys(baseItem)
+				.map(async k => updateItem(k, await cache.match(k), 'xutool-base-page', baseItem[k]))
+		))
+	);
+});
 self.addEventListener('activate', function(event) {});
-let e;
 self.addEventListener('fetch', function(event) {
-	e = event;
 	if (event.request.method !== 'GET') { return; }
 	const path = getPath(event.request.url);
 	if (typeof path !== 'string') { return; }
@@ -47,5 +89,10 @@ self.addEventListener('fetch', function(event) {
 		return event.respondWith(caches.match('./xutool.html'));
 	} else if (pageId) {
 		return event.respondWith(caches.match('./init.html'));
+	}
+	const itemId = getItemId(path);
+	if (itemId) {
+		return event.respondWith(caches.match(`./${itemId}`).then(res => res ? updateItem(itemId, res) : backup(itemId)));
+		
 	}
 });
