3793bb2c01d9d8eff9f2fdf2894c76c6fc7500b1
Wang Chenxu <git@wangchenxu.net>
2019-05-22 10:10:05 +0800

    refactor: 重构Base64算法

--- component/util/base64.js
+++ component/util/base64.js
@@ -1,139 +1,81 @@
 
-//下面是64个基本的编码
-var base64EncodeChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
-var base64DecodeChars = new Array(
-	-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
-	-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
-	-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 62, -1, -1, -1, 63,
-	52, 53, 54, 55, 56, 57, 58, 59, 60, 61, -1, -1, -1, -1, -1, -1,
-	-1,  0,  1,  2,  3,  4,  5,  6,  7,  8,  9, 10, 11, 12, 13, 14,
-	15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, -1, -1, -1, -1, -1,
-	-1, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40,
-	41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, -1, -1, -1, -1, -1);
-//编码的方法
-function base64encode(str) {
-	var out, i, len;
-	var c1, c2, c3;
-	len = str.length;
-	i = 0;
-	out = "";
-	while(i < len) {
-		c1 = str.charCodeAt(i++) & 0xff;
-		if(i == len) {
-			out += base64EncodeChars.charAt(c1 >> 2);
-			out += base64EncodeChars.charAt((c1 & 0x3) << 4);
-			out += "==";
-			break;
+let base64chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/'
+export function base2buff(str, chars = base64chars) {
+	const n = Math.floor(Math.log2(chars.length));
+	str = str.replace(/[=\s]+/g,'');
+	let list = new Uint8Array(str.length * n >> 3);
+	let next = 0;
+	let v = 0, l = 0;
+	for (let c of str) {
+		v = (v << n) | chars.indexOf(c);
+		l += n;
+		while (l >= 8) {
+			l -= 8;
+			list[next++] = (v >> l) & 0xff;
+			v &= (1 << l) - 1;
 		}
-		c2 = str.charCodeAt(i++);
-		if(i == len) {
-			out += base64EncodeChars.charAt(c1 >> 2);
-			out += base64EncodeChars.charAt(((c1 & 0x3)<< 4) | ((c2 & 0xF0) >> 4));
-			out += base64EncodeChars.charAt((c2 & 0xF) << 2);
-			out += "=";
-			break;
+	}
+	return list.buffer;
+}
+export function buff2base(buff, chars = base64chars) {
+	const n = Math.floor(Math.log2(chars.length));
+	const mask = (1 << n) - 1;
+	let list = new Array(Math.floor(((buff.byteLength << 3) + n - 1) / n));
+	let next = 0;
+	let v = 0, l = 0;
+	for (let b of new Uint8Array(buff)) {
+		v = (v << 8) | b;
+		l += 8;
+		while(l >= n) {
+			l -= n;
+			list[next++] = chars[(v >> l) & mask];
+			v &= (1 << l) - 1;
 		}
-		c3 = str.charCodeAt(i++);
-		out += base64EncodeChars.charAt(c1 >> 2);
-		out += base64EncodeChars.charAt(((c1 & 0x3)<< 4) | ((c2 & 0xF0) >> 4));
-		out += base64EncodeChars.charAt(((c2 & 0xF) << 2) | ((c3 & 0xC0) >>6));
-		out += base64EncodeChars.charAt(c3 & 0x3F);
 	}
-	return out;
+	if (l) { list[next++] = chars[(v << (n - l)) & mask]; }
+	while (list.length % 4) { list.push('='); }
+	return list.join('');
 }
-//解码的方法
-function base64decode(str) {
-	var c1, c2, c3, c4;
-	var i, len, out;
-	len = str.length;
-	i = 0;
-	out = "";
-	while(i < len) {
 
-		do {
-			c1 = base64DecodeChars[str.charCodeAt(i++) & 0xff];
-		} while(i < len && c1 == -1);
-		if(c1 == -1){ break; }
-		
-		do {
-			c2 = base64DecodeChars[str.charCodeAt(i++) & 0xff];
-		} while(i < len && c2 == -1);
-		if(c2 == -1) { break; }
-		out += String.fromCharCode((c1 << 2) | ((c2 & 0x30) >> 4));
-
-		do {
-			c3 = str.charCodeAt(i++) & 0xff;
-			if(c3 == 61)
-			return out;
-			c3 = base64DecodeChars[c3];
-		} while(i < len && c3 == -1);
-		if(c3 == -1) { break; }
-		out += String.fromCharCode(((c2 & 0XF) << 4) | ((c3 & 0x3C) >> 2));
-
-		do {
-			c4 = str.charCodeAt(i++) & 0xff;
-			if(c4 == 61)
-			return out;
-			c4 = base64DecodeChars[c4];
-		} while(i < len && c4 == -1);
-		if(c4 == -1) { break; }
-		out += String.fromCharCode(((c3 & 0x03) << 6) | c4);
-	}
-	return out;
-}
-function utf16to8(str) {
-	var out, i, len, c;
-	out = "";
-	len = str.length;
-	for(i = 0; i < len; i++) {
-		c = str.charCodeAt(i);
-		if ((c >= 0x0001) && (c <= 0x007F)) {
-			out += str.charAt(i);
-		} else if (c > 0x07FF) {
-			out += String.fromCharCode(0xE0 | ((c >> 12) & 0x0F));
-			out += String.fromCharCode(0x80 | ((c >>  6) & 0x3F));
-			out += String.fromCharCode(0x80 | ((c >>  0) & 0x3F));
+export function str2buff(str) {
+	let out = [];
+	for (let c of str.split('').map(ch => ch.charCodeAt(0))) {
+		if (c < 0x80) {
+			out.push(c);
+		} else if (c < 0x800) {
+			out.push(0xC0 | 0x1F & (c >> 6));
+			out.push(0x80 | 0x3F & c);
 		} else {
-			out += String.fromCharCode(0xC0 | ((c >>  6) & 0x1F));
-			out += String.fromCharCode(0x80 | ((c >>  0) & 0x3F));
+			out.push(0xE0 | 0x0F & (c >> 12));
+			out.push(0x80 | 0x3F & (c >> 6));
+			out.push(0x80 | 0x3F & c);
 		}
 	}
-	return out;
-}
-function utf8to16(str) {
-	var out, i, len, c;
-	var char2, char3;
-	out = "";
-	len = str.length;
-	i = 0;
-	while(i < len) {
-		c = str.charCodeAt(i++);
-		switch(c >> 4) { 
-			 case 0: case 1: case 2: case 3: case 4: case 5: case 6: case 7:
-				// 0xxxxxxx
-				out += str.charAt(i-1);
-				break;
-				case 12: case 13:
-				// 110x xxxx   10xx xxxx
-				char2 = str.charCodeAt(i++);
-				out += String.fromCharCode(((c & 0x1F) << 6) | (char2 & 0x3F));
-				break;
-			 case 14:
-				// 1110 xxxx  10xx xxxx  10xx xxxx
-				char2 = str.charCodeAt(i++);
-				char3 = str.charCodeAt(i++);
-				out += String.fromCharCode(((c & 0x0F) << 12) |
-										((char2 & 0x3F) << 6) |
-										((char3 & 0x3F) << 0));
-				break;
-		}
-	}
-	return out;
-}
-export function encode(x) {
-	return base64encode(utf16to8(x));
+	return new Uint8Array(out).buffer;
 }
 
-export function decode(x) {
-	return utf8to16(base64decode(x));
+export function buff2str(buffer) {
+	const code = new Uint8Array(buffer);
+	let ret = [];
+	let index = 0;
+	while (index < code.length) {
+		let c = code[index++] | 0;
+		if (c > 0b11100000) {
+			c = (c & 0xF) << 12;
+			c |= (code[index++] & 0x3F) << 6;
+			c |= code[index++] & 0x3F;
+		} else if (c > 0b11000000) {
+			c = (c & 0x1F) << 12;
+			c |= (code[index++] & 0x3F) << 6;
+		}
+		ret.push(c);
+	}
+	return ret.map(x => String.fromCharCode(x)).join('');
+}
+export function encode(str) {
+	return buff2base(str2buff(str));
+}
+
+export function decode(base) {
+	return buff2str(base2buff(base));
 }
